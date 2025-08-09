0f8078be790f15030bc6d7ac25a424a612583425
ttadesse627 <ttadesse627@gmail.com>
2025-08-09 23:42:24 +0300

    Fixed student and course relationship

--- src/ClassScheduler.Domain/Model/Entities/StudentCourse.cs
+++ src/ClassScheduler.Domain/Model/Entities/StudentCourse.cs
@@ -3,17 +3,12 @@ using ClassScheduler.Domain.Model.Entities.Base;
 namespace ClassScheduler.Domain.Model.Entities;
 public class StudentCourse: BaseAuditableEntity
 {
-    public decimal Quiz { get; set; }
-    public decimal Test { get; set; }
-    public decimal MidExam { get; set; }
-    public decimal AssignmentExam { get; set; }
-    public decimal FinalExam { get; set; }
-    public string? Remark { get; set; }
-
     public Guid StudentId { get; set; }
-    public Student Student { get; set; }
+    public required Student Student { get; set; }
     public Guid CourseId { get; set; }
-    public Course Course { get; set; }
-    public Guid GradeId { get; set; }
-    public Grade Grade { get; set; }
+    public required Course Course { get; set; }
+    public Guid? ExamId { get; set; }
+    public Grade? Exam { get; set; }
+    public Guid? GradeId { get; set; }
+    public Grade? Grade { get; set; }
 }
